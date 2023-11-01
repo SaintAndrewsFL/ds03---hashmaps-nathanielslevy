a0054a2cdcf2d92ff7ef51d521d5e3532e3f10c6
natelevy <nathaniel.levy@saintandrews.net>
2023-11-01 09:57:52 -0400

    v1

--- src/com/company/MyHashMap.java
+++ src/com/company/MyHashMap.java
@@ -1,65 +1,192 @@
 package com.company;
 
-public class MyHashMap {
+import java.sql.SQLOutput;
+import java.util.LinkedList;
+import java.util.ArrayList;
 
+public class MyHashMap<N,T> {
+    private LinkedList<Node>[] map;
+    double loadFactor;
+    int capacity;
+    int numFilled = 0;
 
-    public void empty() { //Empties the HashMap of all entries
-
+    public MyHashMap() {
+        map = new LinkedList[16];
+        loadFactor = 0.75;
+        capacity = 16;
+        for (int i = 0; i < map.length; i++) {
+            LinkedList<Node> newLinked = new LinkedList<Node>();
+            map[i] = newLinked;
+        }
     }
 
-    public boolean isEmpty() { //I mean....ya know.
-        return false;
+    public MyHashMap(double loadFactor, int capacity) {
+        map = new LinkedList[capacity];
+        this.loadFactor = loadFactor;
+        this.capacity = capacity;
+        for (int i = 0; i < map.length; i++) {
+            LinkedList<Node> newLinked = new LinkedList<>();
+            map[i] = newLinked;
+        }
     }
 
-    public void put(Object key, Object value) { //The HashMap equivalent of add()
 
+    public void empty() {
+        for (int i = 0; i < map.length; i++) {
+            LinkedList<Node> newLinked = new LinkedList<>();
+            map[i] = newLinked;
+        }
     }
 
-    public void putIfAbsent(Object key, Object value) { //Only places in HashMap if no current entry exists. Does NOT overwrite.
-
+    public boolean isEmpty() {
+        return map.length == 0;
     }
 
-    public Object remove(Object key, Object value) { //Returns the item being removed
+    public void put(N key, T value) {
+        int index = calculateIndex(key);
+        Node node = new Node(key, value);
+        map[index].add(node);
+        if (map[index].size() == 1) {
+            numFilled++;
+        }
+        if (numFilled > loadFactor * capacity) {
+            capacity*=2;
+            for (LinkedList<Node> nodes : map) {
+                for (Node j : nodes) {
+                    N k = j.getKey();
+                    T v = j.getValue();
+                    index = calculateIndex(k);
+                    LinkedList<Node>[] arr = new LinkedList[capacity];
+                    Node tempNode = new Node(k, v);
+                    map[index].add(tempNode);
+                }
+            }
+        }
+    }
+
+
+    public T putIfAbsent(N key, T value) {
+        int index = calculateIndex(key);
+        for (Node j : map[index]) {
+            if (j.getValue().equals(value));
+                return value;
+        }
+        return null;
+    }
+
+    public Node remove(Object key, Object value) { //Returns the item being removed
+        for (LinkedList<Node> nodes : map) {
+            for (Node j : nodes) {
+                if (j.getKey().equals(key) && j.getValue().equals(value)) {
+                    nodes.remove(j);
+                    return j;
+                }
+            }
+        }
         return null;
     }
 
     public boolean containsValue(Object value) { //Will need to search all buckets - think about why.
+        for (LinkedList<Node> nodes : map) {
+            for (Node j : nodes) {
+                if (j.getValue().equals(value)) {
+                    return true;
+                }
+            }
+        }
         return false;
     }
 
     public boolean containsKey(Object key) { //Will search only one bucket.
+        for (LinkedList<Node> nodes : map) {
+            for (Node j : nodes) {
+                if (j.getKey().equals(key)) {
+                    return true;
+                }
+            }
+        }
         return false;
     }
 
     public Object get(Object key) { //Only searches one bucket.
+        int index = calculateIndex(key);
+        for (Node node : map[index]) {
+            if (node.getKey().equals(key)) {
+                return node;
+            }
+        }
         return null;
     }
 
     public Object getOrDefault(Object key, Object value) { //Searches for key and returns respective value if found, otherwise returns 2nd parameter
-        return null;
+        int index = calculateIndex(key);
+        for (Node node : map[index]) {
+            if (node.getKey().equals(key)) {
+                return value;
+            }
+        }
+        return value;
     }
 
     public int size() { //Returns num of items in total HashMap.
-        return 0;
+        int size = 0;
+        for (LinkedList<Node> nodes : map) {
+            for (Node j : nodes) {
+                size++;
+                }
+            }
+        return size;
     }
 
-    public Object replace(Object key, Object value) { //Overwrites existing key/value. Return is old object. If no object exists, should output a message indicating so and do nothing.
+    public Object replace(N key, T value) { //Overwrites existing key/value. Return is old object. If no object exists, should output a message indicating so and do nothing.
+        for (LinkedList<Node> nodes : map) {
+            for (Node j : nodes) {
+            if (j.getKey().equals(key)) {
+                j.setValue(value);
+                }
+            }
+        }
+        System.out.println("Key not present");
         return null;
+
     }
 
     public void display() { //Output entire HashMap. Should indicate which bucket each element belongs to and, if multiple per bucket, starts at head and goes to tail.
+        for (LinkedList<Node> nodes : map) {
+            for (Node j : nodes) {
+                System.out.print("[" + j.getKey() + ", " + j.getValue() + "], ");
+            }
+            System.out.println();
+        }
 
     }
 
-    public int calculateIndex(Object key) { //You'll need this for determining which bucket to go to when given a key.
-        return 0;
-        //Formula is: index = hashCode(key) & (n-1).
+    public int calculateIndex(Object key) {
+        return key.hashCode() & map.length - 1;
     }
-
 
 
     private class Node {
-        //This class will have 4 instance variables
-        //Functionally VERY similar to a LinkedList.
+        private Node next;
+        private N key;
+        private T value;
+
+        public Node(N key, T value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        public T getValue() {
+            return value;
+        }
+
+        public N getKey() {
+            return key;
+        }
+
+        public void setValue(T value) {
+            this.value = value;
+        }
     }
 }
+
